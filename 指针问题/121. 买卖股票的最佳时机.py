5982378c4584e4625178499b8c7492ca931b17d1
Yulin-Lynn <yulin_uxd@outlook.com>
2021-01-08 23:06:26 +0800

    2021.01.09

---
+++ 指针问题/121. 买卖股票的最佳时机.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        n = len(prices)
+        if n == 0: return 0 # 边界条件
+        dp = [0] * n
+        minprice = prices[0] 
+
+        for i in range(1, n):
+            minprice = min(minprice, prices[i])
+            dp[i] = max(dp[i - 1], prices[i] - minprice)
+
+        return dp[-1]
