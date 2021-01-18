79dbd274b2169495116848e2c8b78b218044c71d
Yulin-Lynn <yulin_uxd@outlook.com>
2021-01-18 22:42:38 +0800

    1

---
+++ liteblog/src/main/java/com/yulin/liteblog/LiteblogApplication.java
@@ -0,0 +1,13 @@
+package com.yulin.liteblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteblogApplication.class, args);
+    }
+
+}
---
+++ liteblog/src/test/java/com/yulin/liteblog/LiteblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yulin.liteblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
